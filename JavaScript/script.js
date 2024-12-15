f7e27cb7d7e057c439adbd0a064e021d825b0ac0
Richard Zhao <richardzhaoforwork@gmail.com>
2024-12-16 12:10:36 +1300

    Update script.js

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,18 +1,23 @@
+// Icons Feather Library
+feather.replace();
+
+
+
+// Navbar scroll animation
 const sections = document.querySelectorAll("section");
-const navLinks = document.querySelectorAll(".navbar__item");
+const navLinks = document.querySelectorAll("nav ul li");
 
 const observerOptions = {
   root: null, 
   threshold: 0.7, 
 };
-
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     const id = entry.target.getAttribute("id");
     if (entry.isIntersecting) {
       navLinks.forEach((link) => link.classList.remove("active"));
       const activeLink = document.querySelector(
-        `.navbar__link[href="#${id}"]`
+        `nav a[href="#${id}"]`
       ).parentElement;
       activeLink.classList.add("active");
     }
@@ -23,11 +28,13 @@ sections.forEach((section) => observer.observe(section));
 
 
 
-feather.replace();
-
+// Footer Copyright Date
 const year = new Date().getFullYear();
 document.getElementById("copyright-year").textContent = year;
 
+
+
+// Background tsparticles config
 const options = {
   fullScreen: {
     enable: false,
